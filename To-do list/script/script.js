d35fe7fb5e8a6e858497e9a6ece0c6006ec11f2d
Shubham Anand <shubhamsheelam@gmail.com>
2023-08-20 01:34:47 +0530

    commit

--- To-do list/script/script.js
+++ To-do list/script/script.js
@@ -1,54 +1,70 @@
 $(document).ready(function () {
 
-     $('#Hamburger').click(function () {
+     $("#Hamburger").click(function () {
           $("#dashBoard").toggle(500);
      });
 
-     $('#taskBar').click(function () {
+     $("#taskBar").click(function () {
           $("#task").toggle(500);
      });
         
-     $('#addTaskButton').click(function () {
+     $("#addTaskButton").click(function () {
           $("#addTask").toggle(500);
      });
      
 });
 
+//date setting starts
+
 let dueDate = document.getElementById("dueDate");
 let duedateValue = document.getElementById("duedateValue");
 
-dueDate.addEventListener('change', () => {
-     duedateValue.innerHTML = dueDate.value;
-     console.log(dueDate.value)
+dueDate.addEventListener("change", () => {
+  duedateValue.innerHTML = dueDate.value;
+  console.log(dueDate.value)
 });
 
-const logoutBox = document.getElementById('logoutBox');
-const proccedLogout = document.getElementById('proccedLogout');
-const cancelLogout = document.getElementById('cancelLogout');
+//date setting ends
+
+
+
+
+const logoutBox = document.getElementById("logoutBox");
+const proccedLogout = document.getElementById("proccedLogout");
+const cancelLogout = document.getElementById("cancelLogout");
 
 $(document).ready(function () {
-  $('#logOut').click(() => $('#logoutBox').toggle('fast'));
+  $("#logOut").click(() => $("#logoutBox").toggle("fast"));
 });
 
-proccedLogout.addEventListener('click', () => handleLogout('yes'));
-cancelLogout.addEventListener('click', () => handleLogout('no'));
+proccedLogout.addEventListener("click", () => handleLogout("yes"));
+cancelLogout.addEventListener("click", () => handleLogout("no"));
 
 function handleLogout(status) {
   console.log(status);
-  if (status == 'yes' || status == 'no') {
-    $('#logoutBox').toggle('fast');
+  if (status == "yes" || status == "no") {
+    $("#logoutBox").toggle("fast");
   }
 }
 
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape') {
-    $('#logoutBox').hide('fast');
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    $("#logoutBox").hide("fast");
   }
 });
 
-var priorityBox = document.getElementById('Priority');
-var priorityLevel = document.getElementById('priorityLevel');
+
+
+
+
+
+// priorityBox starts
+
+var priorityBox = document.getElementById("Priority");
+var priorityLevel = document.getElementById("priorityLevel");
 priorityBox
-.addEventListener('change' , ()=>{
+.addEventListener("change" , ()=>{
   priorityLevel.innerHTML = priorityBox.value;
 })
+
+// priorityBox ends
