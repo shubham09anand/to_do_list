1698a3c1b1e264cbf552eef43e3a1bcec71edca9
Shubham Anand <shubhamsheelam@gmail.com>
2023-08-22 01:45:36 +0530

    commit backend 6

--- To-do list/Database/to-do_list.sql
+++ To-do list/Database/to-do_list.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 22, 2023 at 09:06 AM
+-- Generation Time: Aug 21, 2023 at 08:13 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -24,6 +24,64 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `stickytable`
+--
+
+DROP TABLE IF EXISTS `stickytable`;
+CREATE TABLE IF NOT EXISTS `stickytable` (
+  `stickyID` int NOT NULL AUTO_INCREMENT,
+  `UserId` int NOT NULL,
+  `stickyTiltle` varchar(100) NOT NULL,
+  `stickyDescription` text NOT NULL,
+  `stickyTime` time NOT NULL,
+  `stickyDate` date NOT NULL,
+  PRIMARY KEY (`stickyID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `stickytable`
+--
+
+INSERT INTO `stickytable` (`stickyID`, `UserId`, `stickyTiltle`, `stickyDescription`, `stickyTime`, `stickyDate`) VALUES
+(1, 1, 'Sticky1', 'Play', '01:39:00', '2023-08-22'),
+(2, 1, 'sticky2', 'play', '01:41:00', '2023-08-22'),
+(3, 1, 'sticky3', 'play', '01:41:00', '2023-08-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasktable`
+--
+
+DROP TABLE IF EXISTS `tasktable`;
+CREATE TABLE IF NOT EXISTS `tasktable` (
+  `taskID` int NOT NULL AUTO_INCREMENT,
+  `UserId` int NOT NULL,
+  `taskTiltle` varchar(100) NOT NULL,
+  `taskDescription` text NOT NULL,
+  `taskdueDate` date NOT NULL,
+  `taskPriority` varchar(100) NOT NULL,
+  `taskType` varchar(100) NOT NULL,
+  `taskTime` time NOT NULL,
+  `taskDate` date NOT NULL,
+  `taskStatus` varchar(100) NOT NULL,
+  PRIMARY KEY (`taskID`)
+) ENGINE=MyISAM AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='this table is related to user task';
+
+--
+-- Dumping data for table `tasktable`
+--
+
+INSERT INTO `tasktable` (`taskID`, `UserId`, `taskTiltle`, `taskDescription`, `taskdueDate`, `taskPriority`, `taskType`, `taskTime`, `taskDate`, `taskStatus`) VALUES
+(59, 1, 'Home', 'Cleaning', '2023-08-18', 'Priority 4', 'Home', '01:04:00', '2023-08-22', 'Not Completed'),
+(60, 1, 'Lesiure', 'Movies', '2023-08-23', 'Priority 4', 'Lesiure', '01:05:00', '2023-08-22', 'Not Completed'),
+(57, 1, 'Personal', 'sleep', '2023-08-23', 'Priority 2', 'Personal', '01:03:00', '2023-08-22', 'Not Completed'),
+(58, 1, 'Study', 'Maths', '2023-08-11', 'Priority 4', 'study', '01:04:00', '2023-08-22', 'Not Completed'),
+(56, 1, 'Tooday', 'Sleeping', '2023-08-22', 'Priority 2', 'Today', '01:01:00', '2023-08-22', 'Not Completed');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userinfo`
 --
 
@@ -34,16 +92,17 @@ CREATE TABLE IF NOT EXISTS `userinfo` (
   `userPassword` varchar(100) NOT NULL,
   `userEmail` varchar(100) NOT NULL,
   `userJoinedOn` date NOT NULL,
+  `userJoinTime` time NOT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `userinfo`
 --
 
-INSERT INTO `userinfo` (`userID`, `userName`, `userPassword`, `userEmail`, `userJoinedOn`) VALUES
-(9, 'Joe Biden', '$2y$10$qLob5Wh5Gd26b7k.HydUle/mph6vjfhbZXJ07jBgRytvmuPMgxTmW', 'joe@gmail.com', '2023-07-21'),
-(8, 'Shubham Aanad', 'Shubham', 'shubhamanand@gmail.com', '2023-07-21');
+INSERT INTO `userinfo` (`userID`, `userName`, `userPassword`, `userEmail`, `userJoinedOn`, `userJoinTime`) VALUES
+(41, 'Shubham Anand', '@12345', 'shubhamshelelam@gmail.com', '2023-08-19', '21:57:00'),
+(40, 'sadsa', 'qwedqw', 'qweqwe12', '2023-08-19', '21:46:00');
 
 -- --------------------------------------------------------
 
